6aa90576c6b98da3f7031f4218ac23559fbc536f
Yohana <yohana.contreras@cleteci.com>
2023-11-24 14:24:24 -0400

    rendering variables in controller

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,5 +10,10 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'todoapp';
+  welcome = 'Hola!';
+  tasks = [
+    'Instalar angular CLI',
+    'Crear proyecto',
+    'Crear Componentes'
+  ]
 }
